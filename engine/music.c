03dddaf25da61ce84a97aa933e9bb44362b3d0fc
Leeyoukjin <ukjinlee66@gmail.com>
2020-07-21 21:37:27 +0900

    bgm fin

--- engine/music.c
+++ engine/music.c
@@ -6,7 +6,7 @@
 /*   By: youlee <youlee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/21 20:50:30 by youlee            #+#    #+#             */
-/*   Updated: 2020/07/21 21:29:15 by youlee           ###   ########.fr       */
+/*   Updated: 2020/07/21 21:36:23 by youlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ void		startbgm(t_cub *cub)
 {
 	if (SDL_Init(SDL_INIT_AUDIO) < 0)
 		print_str(SDL_GetError());
+	Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 1024);
 	cub->bgm = Mix_LoadMUS("Music/superbgm.mp3");
 	Mix_PlayMusic(cub->bgm, -1);
 }
